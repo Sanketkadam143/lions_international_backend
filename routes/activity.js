bb3c339713edca5bf1a235cf49c2acafeb618ed3
Sanket <sanketanilkadam@gmail.com>
2023-03-16 09:48:41 +0530

    added reported activity

--- routes/activity.js
+++ routes/activity.js
@@ -1,12 +1,13 @@
 import express from "express";
-import {getActivity,getSubtype,getCategory, addActivity, getPlaceholder} from "../controllers/activity.js";
+import {getActivity,getSubtype,getCategory, addActivity, getPlaceholder,getReportedActivity} from "../controllers/activity.js";
 import auth from '../middleware/auth.js';
 const  router=express.Router();
 
 router.get('/type',getActivity);
 router.get('/subtype',getSubtype);
 router.get('/category',getCategory);
-router.get('/placeholder',getPlaceholder)
-router.post('/addactivity',addActivity);
+router.get('/placeholder',getPlaceholder);
+router.get('/reportedactivity',auth,getReportedActivity);
+router.post('/addactivity',auth,addActivity);
 
 export default router;
