81c3165b222be9c82783db671e786456ffe5dc11
Sanket <sanketanilkadam@gmail.com>
2023-03-20 11:55:21 +0530

    fixed minor bugs

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signIn} from "../controllers/login.js";
+import { signIn,resetPassword} from "../controllers/login.js";
 import auth from '../middleware/auth.js';
 
 const  router=express.Router();
 
 router.post('/login',signIn);
+router.post('/resetpass',auth,resetPassword);
 
 
 export default router;
