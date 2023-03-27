42efa0270c2aa2d7fe0f5b2a14d00aa547995788
Sanket <sanketanilkadam@gmail.com>
2023-03-27 17:19:14 +0530

    setup multer and sharp for image upload

--- controllers/news.js
+++ controllers/news.js
@@ -1,26 +1,49 @@
 import connection from "../config/dbconnection.js";
+import sharp from "sharp";
+import path from "path";
 const db = await connection();
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export const reportedNews = async (req, res) => {
-    const clubId=req.clubId;
-    console.log(clubId);
+  const clubId = req.clubId;
   try {
     const sql = "SELECT * FROM news WHERE clubId=?";
-    const data = await db.promise().query(sql,[clubId]);
-   
+    const data = await db.promise().query(sql, [clubId]);
+
     return res.status(200).json(data[0]);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
-
 export const newsReporting = async (req, res) => {
-  const clubId=req.clubId;
+  const clubId = req.clubId;
+  const authorId = req.userId;
+  const verified = 0;
+  const { newsTitle, newsPaperLink, date, description, image } = req.body;
   try {
-    const sql = "SELECT * FROM news WHERE clubId=?";
-    const data = await db.promise().query(sql,[clubId]);
-    return res.status(200).json(data[0]);
+    const imagePath = `/images/news/${req.file.originalname}`;
+    const folder = path.resolve(__dirname, "..") + imagePath;
+    await sharp(req.file.buffer).png().toFile(folder);
+
+    const sql =
+      "INSERT INTO news (clubId, authorId, verified, newsTitle, newsPaperLink, date, description, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    await db
+      .promise()
+      .query(sql, [
+        clubId,
+        authorId,
+        verified,
+        newsTitle,
+        newsPaperLink,
+        date,
+        description,
+        imagePath,
+      ]);
+    return res
+      .status(200)
+      .json({ successMessage: "News inserted successfully" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import connection from "./config/dbconnection.js";
 import * as dotenv from "dotenv";
 import auth from "./routes/login.js";
-import user from "./routes/user.js";
 import activity from './routes/activity.js';
 import adminReport from './routes/adminReports.js';
 import news from './routes/news.js';
@@ -27,8 +26,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/api/auth", auth);
 app.use("/api/activity",activity);
-app.use("/api/user",user);
-app.use("/api/news",news)
+app.use("/api/adminreporting",adminReport)
+app.use("/api/news",news);
+
 app.listen(PORT, () =>
   console.log(`Server Running on Port: http://localhost:${PORT}`)
 );
@@ -38,4 +38,3 @@ process.on('SIGINT', () => {
   db.end();
   process.exit();
 });
-
