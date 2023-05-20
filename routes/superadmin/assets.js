bd57a09a5c8f48efed99e3cee45e491094eaaaa7
Sanket <sanketanilkadam@gmail.com>
2023-05-21 00:50:06 +0530

    minor fix

--- routes/superadmin/assets.js
+++ routes/superadmin/assets.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { addGallery } from "../../controllers/assets.js";
+import superAuth from "../../middleware/superAdmin.js";
 import upload from "../../middleware/imageMulter.js";
 const  router=express.Router();
-router.post('/addGallery',upload.single("image"),addGallery);
+
+router.post('/addGallery',superAuth,upload.single("image"),addGallery);
+
 export default router;
